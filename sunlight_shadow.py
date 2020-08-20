7a9170b7ed0dab527e654b2e37a7d98ad0e00217
renukagajghat <renukagajghat123@gmail.com>
2020-08-20 19:56:51 +0530

    final commit

--- sunlight_shadow.py
+++ sunlight_shadow.py
@@ -7,69 +7,79 @@ def getAngle(a, b, c):
 
 if __name__ == "__main__":
     #accept the input
-    #case2
-    # co_orinates=[[[4,0],[4,-5],[7,-5],[7,0]],[[0.4,-2],[0.4,-5],[2.5,-5],[2.5,-2]]]
-    # sun_co_orinates=[-3.5,1]
-    #case1
+  
     
-    l=[];
-    l=input().split(" ")
-    n=len(l)
-    co_orinates=[]
+    temp=[float(i) for i in input().split()]
+    temp1=[]
+    temp2=[]
+    for i in range(len(temp)):
+        if i % 2 != 0:
+            temp2.append(temp[i])
+            temp1.append(temp2)
+            temp2=[]
+
+        else:
+            temp2.append(temp[i])    
+    
     k=0
-    new_l=[]
+    temp=temp1
+    temp1=[]
+    temp2=[]
+   
+    for i in range(len(temp)):
+        if k==3:
+            temp2.append(temp[i])
+            temp1.append(temp2)
+            temp2=[]
+            k=-1
 
-    for i in range(0,int(n/2)):
-        a=[]
-        for j in range(0,2):
-            a.append(float(l[k]))
-            k=k+1
-        new_l.append(a)
-    co_orinates.append(new_l)
+        else:
+            temp2.append(temp[i])   
+        k=k+1 
+    
+    cordination=temp1
+    point=[float(i) for i in input().split()]
+    
 
-    x,y=input().split(" ")
-    sun_co_orinates=[]
-    sun_co_orinates.append(int(x))
-    sun_co_orinates.append(int(y))
-
-    #print(co_orinates)
-    #print(sun_co_orinates)
-        
-    #co_orinates=[[[4,0],[4,-5],[7,-5],[7,0]]]
-    #sun_co_orinates=[1,1]
-
-
+    #case1        
+    # cordination=[[[4,0],[4,-5],[7,-5],[7,0]]]
+    # point=[1,1]
+ 
+    #case2
+    # cordination=[[[4,0],[4,-5],[7,-5],[7,0]],[[0.4,-2],[0.4,-5],[2.5,-5],[2.5,-2]]]
+    # point=[-3.5,1]
+ 
     #sorting logic
-    for i in range(len(co_orinates)):
-        for j in range(i+1,len(co_orinates)):
-            if co_orinates[i][0][0] > co_orinates[j][0][0]:
-                temp=co_orinates[i]
-                co_orinates[i]=co_orinates[j]
-                co_orinates[j]=temp
+    for i in range(len(cordination)):
+        for j in range(i+1,len(cordination)):
+            if cordination[i][0][0] > cordination[j][0][0]:
+                temp=cordination[i]
+                cordination[i]=cordination[j]
+                cordination[j]=temp
 
     final_ans=0
     sun_angle=-1
 
-    p0=co_orinates[0][0]
-    p1=co_orinates[0][1]
-    p2=co_orinates[0][2]
+    p0=cordination[0][0]
+    p1=cordination[0][1]
+    p2=cordination[0][2]
     height=abs(abs(p0[1])-abs(p1[1]))
     width=abs(abs(p2[0])-abs(p0[0]))
     ans=float(height+width)
 
     final_ans=final_ans+ans
-    for i in range(len(co_orinates)-1):
-        sun_angle=getAngle((sun_co_orinates[0], co_orinates[i][3][0]), sun_co_orinates, co_orinates[i][3])
+    
+    for i in range(len(cordination)-1):
+        sun_angle=getAngle((point[0], cordination[i][3][0]), point, cordination[i][3])
         
-        
-        p1x=co_orinates[i][2][0]
-        p2x=co_orinates[i+1][1][0]
+        p1x=cordination[i][2][0]
+        p2x=cordination[i+1][1][0]
         ans=abs(abs(p1x)-abs(p2x))
         ans=ans*math.tan(math.radians(sun_angle))
-        diff=ans-abs(abs(co_orinates[i][2][1])-abs(co_orinates[i][3][1]))
+        diff=ans-abs(abs(cordination[i][2][1])-abs(cordination[i][3][1]))
         
-        diff1=abs(abs(co_orinates[i+1][0][1])-abs(co_orinates[i+1][1][1]))
-        diff2=abs(abs(co_orinates[i+1][0][0])-abs(co_orinates[i+1][3][0]))
+        diff1=abs(abs(cordination[i+1][0][1])-abs(cordination[i+1][1][1]))
+        diff2=abs(abs(cordination[i+1][0][0])-abs(cordination[i+1][3][0]))
         ans=abs(diff1-diff)+diff2
         final_ans=final_ans+ans
 
